0594d6a0ced78b30bba689a5263c87167f5643a4
Marco Mazzai <mazzai.marco@gmail.com>
2023-09-27 21:53:04 +0200

    ( index ): add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPerson, faRobot } from "@fortawesome/free-solid-svg-icons";
+
 import Link from "next/link";
 
 const HUMAN_VS_COMPUTER_ROUTE = "/game?mode=human-vs-computer";
@@ -6,20 +9,27 @@ const COMPUTER_VS_COMPUTER_ROUTE = "/game?mode=computer-vs-computer";
 const Index: React.FunctionComponent = () => {
   const indexText = {
     heading: "Carta, sasso e forbice",
-    buttonHumanComputer: "Umano contro Computer",
-    buttonComputerComputer: "Computer contro Computer",
+    vs: "vs",
   };
 
-  const { heading, buttonHumanComputer, buttonComputerComputer } = indexText;
+  const { heading, vs } = indexText;
 
   return (
     <div>
       <h1>{heading}</h1>
       <Link href={HUMAN_VS_COMPUTER_ROUTE}>
-        <button>{buttonHumanComputer}</button>
+        <button>
+          <FontAwesomeIcon icon={faPerson} />
+          <span>{vs}</span>
+          <FontAwesomeIcon icon={faRobot} />
+        </button>
       </Link>
       <Link href={COMPUTER_VS_COMPUTER_ROUTE}>
-        <button>{buttonComputerComputer}</button>
+        <button>
+          <FontAwesomeIcon icon={faRobot} />
+          <span>{vs}</span>
+          <FontAwesomeIcon icon={faRobot} />
+        </button>
       </Link>
     </div>
   );
